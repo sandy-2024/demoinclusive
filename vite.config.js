503f2da31089b98d8c633090ed4e53790faf8502
sandy-2024 <sandesh3740@gmail.com>
2024-06-15 11:37:39 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      'react-router-dom': '/node_modules/react-router-dom'
-    }
-  }
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],
+    },
+  },
 });
